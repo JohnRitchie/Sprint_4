3f7ba8c809ecaf911a4d47234f2c12850bb07912
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2022-10-21 11:46:41 +0300

    links

---
+++ links.py
@@ -0,0 +1,4 @@
+YA_PAGE_LINK = 'https://dzen.ru/?yredirect=true'
+MAIN_PAGE_LINK = 'https://qa-scooter.praktikum-services.ru/'
+ORDER_PAGE_LINK = 'https://qa-scooter.praktikum-services.ru/order'
+STATUS_PAGE_LINK = 'https://qa-scooter.praktikum-services.ru/track?t={}'
