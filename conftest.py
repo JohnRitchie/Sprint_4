16e4d2021a6b7e19724c97d21dd25a7b601b9651
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2022-11-01 13:53:39 +0300

    add headless

--- conftest.py
+++ conftest.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 @pytest.fixture
 def browser():
     print("\nstart browser for test..")
+    # options = webdriver.FirefoxOptions()
+    # options.add_argument('--headless')
+    # browser = webdriver.Firefox(options=options)
     browser = webdriver.Firefox()
     yield browser
     print("\nquit browser for test..")
