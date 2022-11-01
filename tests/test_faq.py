dedb145e41a50bd0c652ff99e31dde118eed3b9d
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2022-11-01 13:46:37 +0300

    parametrize

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -1,113 +1,56 @@
+import pytest
+
 from page_objects.main_page import MainPage
 from links import MAIN_PAGE_LINK
 from locators.main_page_locators import MainPageLocators
 
 
-def test_faq_click_first_item_open_first_panel_text(browser):
+@pytest.mark.parametrize('item_heading, expected_text, panel',
+                         [(MainPageLocators.FIRST_FAQ_ITEM_HEADING,
+                           'Сутки — 400 рублей. Оплата курьеру — наличными или картой.',
+                           MainPageLocators.FIRST_FAQ_ITEM_PANEL),
+                          (MainPageLocators.SECOND_FAQ_ITEM_HEADING,
+                           'Пока что у нас так: один заказ — один самокат. '
+                           'Если хотите покататься с друзьями, '
+                           'можете просто сделать несколько заказов — один за другим.',
+                           MainPageLocators.SECOND_FAQ_ITEM_PANEL),
+                          (MainPageLocators.THIRD_FAQ_ITEM_HEADING,
+                           'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. '
+                           'Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. '
+                           'Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.',
+                           MainPageLocators.THIRD_FAQ_ITEM_PANEL),
+                          (MainPageLocators.FOURTH_FAQ_ITEM_HEADING,
+                           'Только начиная с завтрашнего дня. Но скоро станем расторопнее.',
+                           MainPageLocators.FOURTH_FAQ_ITEM_PANEL),
+                          (MainPageLocators.FIFTH_FAQ_ITEM_HEADING,
+                           'Пока что нет! '
+                           'Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.',
+                           MainPageLocators.FIFTH_FAQ_ITEM_PANEL),
+                          (MainPageLocators.SIXTH_FAQ_ITEM_HEADING,
+                           'Самокат приезжает к вам с полной зарядкой. '
+                           'Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. '
+                           'Зарядка не понадобится.',
+                           MainPageLocators.SIXTH_FAQ_ITEM_PANEL),
+                          (MainPageLocators.SEVENTH_FAQ_ITEM_HEADING,
+                           'Да, пока самокат не привезли. '
+                           'Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.',
+                           MainPageLocators.SEVENTH_FAQ_ITEM_PANEL),
+                          (MainPageLocators.EIGHTH_FAQ_ITEM_HEADING,
+                           'Да, обязательно. Всем самокатов! И Москве, и Московской области.',
+                           MainPageLocators.EIGHTH_FAQ_ITEM_PANEL),
+                          ])
+def test_faq_click_item_open_panel_text(browser, item_heading, expected_text, panel):
     main_page = MainPage(browser, MAIN_PAGE_LINK)
     main_page.open()
 
-    main_page.click_faq_item_heading(MainPageLocators.FIRST_FAQ_ITEM_HEADING)
+    main_page.click_faq_item_heading(item_heading)
 
-    expected_text = 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
-    actual_text = main_page.get_text(MainPageLocators.FIRST_FAQ_ITEM_PANEL)
+    expected_text = expected_text
 
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_second_item_open_second_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.SECOND_FAQ_ITEM_HEADING)
-
-    expected_text = 'Пока что у нас так: один заказ — один самокат. ' \
-                    'Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.'
-
-    actual_text = main_page.get_text(MainPageLocators.SECOND_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_third_item_open_third_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.THIRD_FAQ_ITEM_HEADING)
-
-    expected_text = 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. ' \
-                    'Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. ' \
-                    'Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.'
-
-    actual_text = main_page.get_text(MainPageLocators.THIRD_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_fourth_item_open_fourth_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.FOURTH_FAQ_ITEM_HEADING)
-
-    expected_text = 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
-
-    actual_text = main_page.get_text(MainPageLocators.FOURTH_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_fifth_item_open_fifth_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.FIFTH_FAQ_ITEM_HEADING)
-
-    expected_text = 'Пока что нет! ' \
-                    'Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.'
-
-    actual_text = main_page.get_text(MainPageLocators.FIFTH_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_sixth_item_open_sixth_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.SIXTH_FAQ_ITEM_HEADING)
-
-    expected_text = 'Самокат приезжает к вам с полной зарядкой. ' \
-                    'Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. ' \
-                    'Зарядка не понадобится.'
-
-    actual_text = main_page.get_text(MainPageLocators.SIXTH_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_seventh_item_open_seventh_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.SEVENTH_FAQ_ITEM_HEADING)
-
-    expected_text = 'Да, пока самокат не привезли. ' \
-                    'Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.'
-
-    actual_text = main_page.get_text(MainPageLocators.SEVENTH_FAQ_ITEM_PANEL)
-
-    assert actual_text == expected_text, 'FAQ item panel is hidden!'
-
-
-def test_faq_click_eighth_item_open_eighth_panel_text(browser):
-    main_page = MainPage(browser, MAIN_PAGE_LINK)
-    main_page.open()
-
-    main_page.click_faq_item_heading(MainPageLocators.EIGHTH_FAQ_ITEM_HEADING)
-
-    expected_text = 'Да, обязательно. Всем самокатов! И Москве, и Московской области.'
-
-    actual_text = main_page.get_text(MainPageLocators.EIGHTH_FAQ_ITEM_PANEL)
+    panel = panel
+    if main_page.is_element_present(panel):
+        actual_text = main_page.get_text(panel)
+    else:
+        actual_text = ''
 
     assert actual_text == expected_text, 'FAQ item panel is hidden!'
